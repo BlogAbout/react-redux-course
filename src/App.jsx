42fb9599fe538b4d9041870cd62545f9a596bd0c
BlogAbout <info@blog-about.ru>
2021-09-18 19:28:38 +0800

    Total learn course React and Redux.

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+import {useDispatch, useSelector} from "react-redux";
+import {asyncDecrementCreator, asyncIncrementCreator} from "./store/countReducer";
+import {fetchUsers} from "./store/userReducer";
+
+function App() {
+    const count = useSelector(state => state.countReducer.count)
+    const users = useSelector(state => state.userReducer.users)
+    const dispatch = useDispatch()
+
+    return (
+        <div className="app">
+            <div className="count">{count}</div>
+            <div className="btns">
+                <button className="btn" onClick={() => dispatch(asyncIncrementCreator())}>Инкремент ++</button>
+                <button className="btn" onClick={() => dispatch(asyncDecrementCreator())}>Декремент --</button>
+                <button className="btn" onClick={() => dispatch(fetchUsers())}>Получить юзеров</button>
+            </div>
+            <div className="users">
+                {users.map(user =>
+                    <div className="user">{user.name}</div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default App;
